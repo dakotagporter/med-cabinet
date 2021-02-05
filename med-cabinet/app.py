1a1c978b9e116c29e4d0a9d7657effaedc02070c
dakotagporter <dakotap3045@gmail.com>
2021-02-05 14:14:28 -0700

    add documentation

--- med-cabinet/app.py
+++ med-cabinet/app.py
@@ -1,23 +1,38 @@
-"""Main app/routing for med-cabinet"""
-from flask import Flask, render_template, request, url_for, redirect, flash
-from .cannabis import *
+from flask import Flask, render_template, request, flash
+from .cannabis import EFFECTS, ERROR, search_strains, query_results
 from .predict import predict
 
 
 def create_app():
+    """
+    This function is the foundation for the Flask API of this project. This
+    function is a cumulation of all application endpoints and configurations.
+    """
     app = Flask(__name__)
+
+    # App configurations
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+    # App endpoints
     @app.route('/', methods=['GET'])
     def index():
+        """
+        Application home page using the base html file.
+        """
         return render_template('index.html', effects=EFFECTS)
 
     @app.route('/results')
     def results():
+        """
+        Renders results html template to provide search results for users.
+        """
         return render_template('results.html')
 
     @app.route('/results', methods=['POST'])
     def results_post():
+        """
+        Deals with any POST methods returned from the base html page.
+        """
         if request.method == 'POST':
             error = None
             # Retrieve user input
@@ -25,41 +40,52 @@ def create_app():
             # Retrieve user search input
             search = request.form.get('search')
 
+            # Search and return strains if user search parameters are provided
             if search:
-                # Search strains based on input
                 values = search_strains(search)
-                
+
                 if values == ERROR:
                     values = ''
                     error = ERROR
-                
-                return render_template('results.html', values=values, search=search, error=error)
 
+                return render_template('results.html', values=values,
+                                       search=search, error=error)
+
+            # Return an error if no effects are chosen by user
             if not effects:
                 flash('Please choose at least one effect')
                 return render_template('index.html', effects=EFFECTS,
                                        error=True)
+
+            # Retrieve strain_type if provided by user
             strain_type = request.form.get('type')
-            # Retrieve description (if applicable)
+            # Retrieve description if provided by user
             description = request.form.get('description')
-            if not description == '':
-                # Return indices of recommended strains based on user description
+
+            # Run model if user description is provided
+            if description != '':
+                # Get strain indices based on user description
                 prediction = predict(description)
                 # Search strains based on user's chosen effects and strain type
                 strains = query_results(effects, strain_type, prediction)
             else:
+                # Search strains based on user's chosen effects and strain type
                 strains = query_results(effects, strain_type, None)
-
 
         return render_template('results.html', strains=strains)
 
     @app.route('/search')
     def search():
+        """
+        Renders search page for users to search strains by name.
+        """
         return render_template('search.html')
 
     @app.route('/about')
     def about():
-        # TODO: Display author information
+        """
+        Display information about project and authors in the about html page.
+        """
         return render_template('about.html')
 
     return app
